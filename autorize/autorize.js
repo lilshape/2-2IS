711c9bbd35022b684222b68d21b4c057c0ddc956
lilshape <kirill.novikov2005@yandex.ru>
2024-04-17 10:08:03 +0300

    не нужно

---
+++ autorize/autorize.js
@@ -0,0 +1,11 @@
+function butclick() {
+let username = "admin";
+let userpass = "12345";
+    if (username && userpass ) {
+      alert("Вы вошли");
+}
+    else 
+{
+      alert("Неправильные данные. Пожалуйста, попробуйте ещё раз.");
+}
+} 
