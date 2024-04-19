e530b66718d2355814d0512d32ec7b187443d8e9
lilshape <kirill.novikov2005@yandex.ru>
2024-04-19 14:20:29 +0300

    добавил 3 массив на определение прав пользователя, но пока неправильно ещё есть над чем работать

--- autorize.js
+++ autorize.js
@@ -4,8 +4,11 @@ function butclick() {
   // для получения элемента поля ввода с идентификатором "username"
   let username = document.getElementById("username").value;
   let userpass = document.getElementById("password").value;
-  let users = [admin, user, manager, user2, user3];
+  let users = [admin, user, admin1, user1, user2];
   let passwords = [123, 456, 789, 987, 654];
+  let typRoot = [a, u, a, u, u];
+  
+  
   // Получаем значения из полей ввода
 
   // делаем проверку совпадают ли данные
@@ -13,25 +16,42 @@ function butclick() {
     if (username === users[i] && userpass === passwords[i]) {
       console.log("Вы вошли в систему.");
   
-      // Скрытие вкладки авторизации
+  // Скрытие вкладки авторизации
       document.getElementById("login-form").style.display = "none";
   
-      // Отображение вкладки с пользователем
+  // Отображение вкладки с пользователем
       document.getElementById("user-panel").style.display = "block";
   
-      // Установка имени пользователя во вкладке с пользователем
+  // Установка имени пользователя во вкладке с пользователем
       document.getElementById("user-name").innerHTML = username;
     alert("Авторизация прошла успешно!");
-    // Выход из функции после авторизации
-  } else {
-    console.log("Неправильные данные. Пожалуйста, попробуйте еще раз.");
+  // Определение типа пользователя
+        if (typeRoot[i] === "a") {
+  // Пользователь является администратором
+    
+          let newPass = prompt("Введите новый пароль:");
+    
+          if (newPass && newPass.length > 0) {
+            passwords[i] = newPass;
+    
+            alert("Пароль успешно изменён.");
+        }
+      }
+        else if (typeRoot[i] === "u") {
+  // Пользователь является обычным пользователем
+          console.log("Вы не можете изменять пароли.");
+        }
+    }
   }
 }
-}
+      
+  else {
+    console.log("Неправильные данные. Пожалуйста, попробуйте еще раз.");
+  }
+
 function logout() {
   // Скрытие вкладки с пользователем
   document.getElementById("user-panel").style.display = "none";
   // Отображение вкладки авторизации
   document.getElementById("login-form").style.display = "block";
 }
-
